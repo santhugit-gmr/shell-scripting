1d9dbdb6e9fe620ec06f5ce2e7cf82b7ae5b7bf6
santhugit-gmr <santhosh.thirumuru1916@gmail.com>
2025-02-27 15:38:34 +0530

    part of bug

---
+++ 03.variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "Ramesh : Hi , Good evening"
+echo "Suresh : Hi , Good evening"
+echo "Ramesh: Hi , Good evening"
+echo "Suresh : Hi , Good evening"
