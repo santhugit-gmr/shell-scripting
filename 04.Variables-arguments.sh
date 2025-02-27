5ddfd094c7c54fce49d2feef548ae29c78f8801a
santhugit-gmr <santhosh.thirumuru1916@gmail.com>
2025-02-27 16:07:27 +0530

    part of bug

---
+++ 04.Variables-arguments.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1 : Hi , Good evening"
+echo "$PERSON2 : Hi , Good evening"
+echo "$PERSON1: Hi , Good evening"
+echo "$PERSON2 : Hi , Good evening"
