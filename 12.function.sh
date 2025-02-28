1c3420f0bebf8e26a855e30aa4baa25697a3a8f0
santhugit-gmr <santhosh.thirumuru1916@gmail.com>
2025-02-28 15:56:58 +0530

    As part of debug

---
+++ 12.function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ID=$(id -u)
+Validation ()
+{
+if [ $? -ne 0 ]
+then
+echo "Error: $2 : installation Failed"
+exit 1
+else
+echo " $2 : installation Success "
+fi
+}
+if [ $ID -ne 0 ]
+then
+echo "Error : Please run this as root user"
+exit 1
+else
+echo "User is root user"
+fi
+yum install mysql -y
+Validation $? "Installing My sql"
+yum install git -y
+Validation $? "Installing My Git"
