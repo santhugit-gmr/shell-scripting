30cfb9f266c38e0b242240a2f1e11c11e5c75746
santhugit-gmr <santhosh.thirumuru1916@gmail.com>
2025-02-28 16:55:42 +0530

    As part of debug

--- 12.function.sh
+++ 12.function.sh
@@ -23,7 +23,7 @@ exit 1
 else
 echo "User is root user"
 fi
-yum install mysqll -y &>> $LOGFILE
+yum install mysql -y &>> $LOGFILE
 Validation $? "Installing My sql"
-yum install git -y &>> $LOGFILE
+yum install gitt -y &>> $LOGFILE
 Validation $? "Installing My Git"
